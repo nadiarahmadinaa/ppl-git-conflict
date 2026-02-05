941cb441a23c7a5578742300bedeaca646c300e8
Ramy Ardya <161923530+UltraRamy@users.noreply.github.com>
2026-02-05 08:29:51 +0700

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -2,8 +2,8 @@ class Calculator:
     def add(self, a, b):
         return a + b
 
-    def subtract(self, a, b):
-        return a - b
+    def subtract(self, ramy, b):
+        return ramy - b
 
     def multiply(self, a, b):
         return a * b
